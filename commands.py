d60108b8192c1046e76a7b42c79b6c3886bd864b
lassmanjm@gmail <lassmanjm@gmail.com>
2025-01-30 15:07:22 -0500

    Rename methods

--- commands.py
+++ commands.py
@@ -5,6 +5,7 @@ from discord.ui import Select, View
 import time
 import asyncio
 import math
+from num2words import num2words
 
 intents = discord.Intents.default()
 intents.members = True
@@ -87,7 +88,7 @@ async def RoundTable(ctx, length: float=5.):
     await channel.send("The time for talk is now over. ")
 
 
-async def murder(victims):
+async def Kill(victims):
     instructions_channel = client.get_channel(constants.instructions_channel_id)
     await instructions_channel.send(embed=discord.Embed(title="The traitors have struck again!", description=f"**{DisplayVictims([victim.display_name for victim in victims])}** {"are" if len(victims)>1 else "is"} dead.", color=discord.Color.red()) )
 # Murder selections
@@ -109,14 +110,14 @@ async def victim_select_callback(interaction: discord.Interaction, view):
 
     victims = [client.get_user(int(victim)) for victim in victim_select.values]
     await interaction.response.send_message(f"You have made your selection. **{DisplayVictims([victim.display_name for victim in victims])}** will no longer be with us.")
-    await murder(victims)
+    await Kill(victims)
 
 @tree.command(
-    name="murder_instructions",
+    name="murder",
     description="Send instructions to murder",
     guild=discord.Object(id=constants.control_guild)
 )
-async def MurderInstructions(ctx, num_victims:int = 1):
+async def Murder(ctx, num_victims:int = 1):
     traitors_channel = client.get_channel(constants.traitors_channel_id)
 
     main_guild = client.get_guild(constants.main_guild)
@@ -143,7 +144,7 @@ async def MurderInstructions(ctx, num_victims:int = 1):
 
     
     victim_select.callback = lambda ctx: victim_select_callback(ctx, view)
-    await ctx.response.send_message("Murder instructions sent.")
+    await ctx.response.send_message("Murder initiated.")
 
 async def RecruitComplete():
     await client.get_channel(constants.control_channel_id).send(embed=discord.Embed(
@@ -153,7 +154,7 @@ async def RecruitComplete():
     ))
     
 
-async def recruit_response_callback(interaction: discord.Interaction, view, selected_user, force:bool):
+async def RecruitResponseCallback(interaction: discord.Interaction, view, selected_user, force:bool):
     recruit_response=None
     for item in view.children:
         if item.custom_id=="recruit_response":
@@ -180,7 +181,7 @@ async def recruit_response_callback(interaction: discord.Interaction, view, sele
                 title=f"{selected_user.display_name} has rejected the offer. They will now be murdered.",
                 color=discord.Color.red()
             ))
-            await murder([selected_user])
+            await Kill([selected_user])
 
         else:
             await interaction.response.send_message(f"You have rejected the offer, and therefore remain faithful.")
@@ -192,7 +193,7 @@ async def recruit_response_callback(interaction: discord.Interaction, view, sele
 
 
     
-async def recruit_select_callback(interaction: discord.Interaction, view, force:bool):
+async def RecruitSelectCallback(interaction: discord.Interaction, view, force:bool):
     recruit_select=None
     for item in view.children:
         if item.custom_id=="recruit_select":
@@ -237,7 +238,7 @@ async def recruit_select_callback(interaction: discord.Interaction, view, force:
         view.add_item(recruit_response)
         
         await selected_recruit.send("Join the traitors?", view=view)
-        recruit_response.callback = lambda ctx: recruit_response_callback(ctx, view, selected_recruit, force)
+        recruit_response.callback = lambda ctx: RecruitResponseCallback(ctx, view, selected_recruit, force)
 
     else:
         traitors_channel = client.get_channel(constants.traitors_channel_id)
@@ -249,7 +250,7 @@ async def recruit_select_callback(interaction: discord.Interaction, view, force:
     
 
 
-async def recruit(force:bool=False):
+async def InitiateRecruit(force:bool=False):
     traitors_channel=client.get_channel(constants.traitors_channel_id)
 
     main_guild = client.get_guild(constants.main_guild)
@@ -266,9 +267,9 @@ async def recruit(force:bool=False):
     await traitors_channel.send(f"Select a player to recruit to be a traitor:", view=view)
 
     
-    recruit_select.callback = lambda ctx: recruit_select_callback(ctx, view, force)
+    recruit_select.callback = lambda ctx: RecruitSelectCallback(ctx, view, force)
 
-async def recruit_decide_callback(interaction: discord.Interaction, view):
+async def RecruitDecideCallback(interaction: discord.Interaction, view):
     recruit_decide=None
     for item in view.children:
         if item.custom_id=="recruit_decide":
@@ -287,7 +288,7 @@ async def recruit_decide_callback(interaction: discord.Interaction, view):
     decision = recruit_decide.values[0]  # The selected user ID
     if decision == "yes":
         await interaction.response.send_message(f"Excellent.")
-        await recruit()
+        await InitiateRecruit()
     else:
         await interaction.response.send_message(f"You have rejected the opportunity to recruit. Your numbers will not change. Await further instructions.")
         await asyncio.sleep(10)
@@ -295,14 +296,14 @@ async def recruit_decide_callback(interaction: discord.Interaction, view):
     
 
 @tree.command(
-    name="rectruit_instructions",
+    name="recruit",
     description="Send instructions to recruit",
     guild=discord.Object(id=constants.control_guild)
 )
-async def RecruitInstructions(ctx, force:bool = False):
+async def Recruit(ctx, force:bool = False):
     if force:
-        await recruit(force=True)
-        await ctx.response.send_message("Recruit instructions sent.")
+        await InitiateRecruit(force=True)
+        await ctx.response.send_message("Recruit initiated.")
         return
     traitors_channel = client.get_channel(constants.traitors_channel_id)
     user_options = [
@@ -326,11 +327,11 @@ async def RecruitInstructions(ctx, force:bool = False):
     await traitors_channel.send(f"Would you like to recruit?", view=view)
 
     
-    recruit_select.callback = lambda ctx: recruit_decide_callback(ctx, view)
-    await ctx.response.send_message("Recruit instructions sent.")
+    recruit_select.callback = lambda ctx: RecruitDecideCallback(ctx, view)
+    await ctx.response.send_message("Recruit initiated.")
     
 # Murder selections
-async def deathmatch_victim_select_callback(interaction: discord.Interaction, view):
+async def DeathmatchVictimSelectCallback(interaction: discord.Interaction, view):
     deathmatch_victim_select=None
     for item in view.children:
         if item.custom_id=="deathmatch_victim_select":
@@ -358,11 +359,11 @@ async def deathmatch_victim_select_callback(interaction: discord.Interaction, vi
         ))
 
 @tree.command(
-    name="deathmatch_instructions",
+    name="deathmatch",
     description="Send instructions for deathmatch",
     guild=discord.Object(id=constants.control_guild)
 )
-async def DeathmatchInstructions(ctx, num_victims:int = 4):
+async def DeathMatch(ctx, num_players:int = 4, num_victims:int = 1):
     traitors_channel = client.get_channel(constants.traitors_channel_id)
 
     main_guild = client.get_guild(constants.main_guild)
@@ -374,23 +375,23 @@ async def DeathmatchInstructions(ctx, num_victims:int = 4):
         custom_id="deathmatch_victim_select",
         placeholder="Select an option",
         options=user_options,
-        max_values=num_victims,
-        min_values=num_victims
+        max_values=num_players,
+        min_values=num_players
         )
     view = View()
     view.add_item(deathmatch_victim_select)
     await traitors_channel.send(
         embed=discord.Embed(
             title="It is time for the Death Match",
-            description="Traitors, you now have a unique opportunity and may send any 4 players to a Death Match, including at most 3 of yourselves. Four will walk in, but only three will walk out.",
+            description=f"Traitors, you now have a unique opportunity and may send any {num_players} players to a Death Match, including at most {num_players-num_victims} of yourselves. {num2words(num_players).capitalize()} will walk in, but only {num2words(num_players-num_victims)} will walk out.",
             color=discord.Color.purple()
             ),
         )
-    await traitors_channel.send(f"Select 4 players to send to the Death Match:", view=view)
+    await traitors_channel.send(f"Select {num_players} players to send to the Death Match:", view=view)
 
     
-    deathmatch_victim_select.callback = lambda ctx: deathmatch_victim_select_callback(ctx, view)
-    await ctx.response.send_message("Death Match instructions sent.")
+    deathmatch_victim_select.callback = lambda ctx: DeathmatchVictimSelectCallback(ctx, view)
+    await ctx.response.send_message("Death Match initiated")
 
 # \Control Commands ---------------------------------------------------------------------------------------------------------------------------
 
@@ -401,7 +402,7 @@ async def DeathmatchInstructions(ctx, num_victims:int = 4):
     description="Test DM all players.",
     guild=discord.Object(id=constants.main_guild)
 )
-async def test(ctx):
+async def DmTest(ctx):
     # Get the guild (server) the command was called from
     guild = ctx.guild
     failed=[]
