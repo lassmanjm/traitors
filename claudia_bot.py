7c1ff7b16258261d3347e817bf30876fa26b4893
lassmanjm <lassmanjm@gmail.com>
2025-04-11 23:13:14 -0400

    format claudia bot

--- claudia_bot.py
+++ claudia_bot.py
@@ -12,41 +12,50 @@ flags.DEFINE_string(
     "bot_token",
     "",
     # TODO add description
-    "description"
+    "description",
 )
 flags.DEFINE_string(
     "server_id",
     "",
     # TODO add description
-    "description"
+    "description",
 )
 
 intents = discord.Intents.default()
 intents.members = True
-intents.guilds = True 
+intents.guilds = True
 intents.guild_messages = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-def main(argv):
-    guild_id=FLAGS.server_id if FLAGS.server_id else os.environ.get('TRAITORS_SERVER_ID')
-    if not guild_id:
-        raise ValueError("Server ID not provided. Set 'server_id' flag or 'TRAITORS_SERVER_ID' environment variable.")
-    guild_id=int(guild_id)
 
-    bot_token=FLAGS.bot_token if FLAGS.bot_token else os.environ.get('TRAITORS_BOT_TOKEN')
+def main(argv):
+    guild_id = (
+        FLAGS.server_id if FLAGS.server_id else os.environ.get("TRAITORS_SERVER_ID")
+    )
+    if not guild_id:
+        raise ValueError(
+            "Server ID not provided. Set 'server_id' flag or 'TRAITORS_SERVER_ID' environment variable."
+        )
+    guild_id = int(guild_id)
+
+    bot_token = (
+        FLAGS.bot_token if FLAGS.bot_token else os.environ.get("TRAITORS_BOT_TOKEN")
+    )
     if not bot_token:
-        raise ValueError("Bot token not provided. Set 'bot_token' flag or 'TRAITORS_BOT_TOKEN' environment variable.")
+        raise ValueError(
+            "Bot token not provided. Set 'bot_token' flag or 'TRAITORS_BOT_TOKEN' environment variable."
+        )
 
     @client.event
     async def on_ready():
         setup_commands.SetupCommands(tree, guild_id, client)
         game_controls.GameControls(tree, guild_id, client)
         await tree.sync(guild=discord.Object(id=guild_id))
-        print(f'Logged in as {client.user}: commands')
+        print(f"Logged in as {client.user}: commands")
 
     client.run(bot_token)
 
+
 if __name__ == "__main__":
     app.run(main)
-
